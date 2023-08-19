7d869cf98bd3407c2f63f2279a442bd25fc5d792
Miguel Angel Peña Marin <138605407+DarthKar@users.noreply.github.com>
2023-08-19 18:12:13 -0500

    Clase auto con Metodos

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,47 @@
+package test;
+
+public class Auto {
+
+	String modelo;
+	int precio;
+	Asiento [] asientos;
+	Motor motor;
+	int registro;
+	static int CantidadCreados;
+	
+	int cantidadAsientos() {
+		
+		int nAsientos=0;
+		for (int i = 0; i<asientos.length; i++) {
+			
+			if(asientos[i] != null)
+			{
+			
+				nAsientos++;
+			}
+		}
+			
+		return nAsientos;
+	}
+	String verificarIntegridad() {
+		
+		if (registro==motor.registro) {		
+			
+			for (int i = 0; i<asientos.length; i++) {
+				
+				if(asientos[i] != null)
+					
+				{
+				if(asientos[i].registro != registro) {
+					
+					return "Las piezas no son originales";	
+				}
+		
+		        }		
+	} 
+		return "Auto original";	 
+		}else {
+		return "Las piezas no son originales";
+	}
+}
+}
