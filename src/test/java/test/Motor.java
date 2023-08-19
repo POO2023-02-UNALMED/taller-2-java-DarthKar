1fc4b07062f3ac6e9cfc17464abdbf9023302d0a
Miguel Angel Peña Marin <138605407+DarthKar@users.noreply.github.com>
2023-08-19 17:19:04 -0500

    Clase motor con Metodos

---
+++ src/test/java/test/Motor.java
@@ -0,0 +1,21 @@
+package test;
+
+public class Motor {
+	
+	int numeroCilindros;
+	String tipo;
+	int registro;
+	
+	void cambiarRegistro(int registro) {
+		
+		this.registro = registro; 			
+	}
+	void cambiartTipo(String tipo) {
+		if (tipo=="electrico" || tipo=="gasolina") {
+			this.tipo = tipo;
+		}
+		
+		
+	}
+	
+}
